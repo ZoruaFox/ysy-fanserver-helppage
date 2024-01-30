c74823d85c4a71b6b68dfdb530e4d1f522f57e58
海屿有燕 <zorua@vip.qq.com>
2024-01-30 16:43:58 +0800

    Add Chinese language support and update base URL and page title

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,13 @@ import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
+  lang: 'zh-CN',
+
+  base: '/YsServer-HelpPage-New/',
+
+  title: '有兽焉粉丝服务器帮助页',
+  description: '阿巴',
+
   bundler: viteBundler(),
   theme: defaultTheme(),
 })
