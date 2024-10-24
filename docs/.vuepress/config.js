c4d100399833f6cf3f022fa905575b82e1c7e565
海屿有燕 <zorua@vip.qq.com>
2024-10-24 17:29:57 +0800

    feat: update shikiPlugin configuration and enhance markdown options in VuePress config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,6 @@ import { hopeTheme } from "vuepress-theme-hope";
 import { navbarConfig } from './configs/navbar.json'
 import { sidebarConfig } from './configs/sidebar.json'
 import { notFoundMsg } from './configs/notFoundMsg.json'
-import { shikiPlugin } from '@vuepress/plugin-shiki'
 
 export default defineUserConfig({
   bundler: viteBundler(), // vite
@@ -51,7 +50,6 @@ export default defineUserConfig({
       //   插件选项
       // },
       mdEnhance: {
-        codetabs: true,
         // 启用下角标功能
         sub: true,
         // 启用上角标
@@ -62,17 +60,15 @@ export default defineUserConfig({
         footnote: true,
         demo: true,
       },
-      tabs: true,
-      imgLazyload: true,
-      figure: true,
-      alert: true,
-      imgMark: true,
-      imgSize: true,
-      copyCode: true,
-      shikiPlugin:{
-        // 配置项
-        langs: ['ts', 'json', 'vue', 'md', 'bash', 'diff', 'python', 'yaml', 'batch', 'cmd', 'java', 'powershell']
+      shiki: {
+        themes: {
+          light: "one-light",
+          dark: "one-dark-pro",
+          },
         },
+      markdownHint: true,
+      markdownImage: true,
+      markdownTab: true,
       pwa: {
         showInstall: true,
         favicon: '/images/favicon/favicon.ico',
@@ -95,6 +91,7 @@ export default defineUserConfig({
           statusBarColor: '#46bd87',
         }
       },
+      copyCode: true,
     },
-  })
+  }),
 })
