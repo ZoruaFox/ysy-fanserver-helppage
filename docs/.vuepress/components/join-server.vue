60449d6f61bb15c651ca1be5813d98b681c2cd18
白给 <2271007263A@gmail.com>
2025-01-21 16:13:24 +0800

    更新首页卡片样式

--- docs/.vuepress/components/join-server.vue
+++ docs/.vuepress/components/join-server.vue
@@ -1,16 +1,25 @@
-<script setup></script>
+<script setup>
+
+const copyContent = async (text) => {
+    await navigator.clipboard.writeText(text)
+    alert('已复制到剪贴板')
+}
+</script>
 <template>
     <div class="join-card-container">
         <div class="server-info join-card">
             <p class="server-name">服务器信息</p>
-            <p class="server-description">地址： mc.zorua.top</p>
+            <p class="server-description">
+                地址：<code><span class="copyable" @click="copyContent">mc.zorua.top</span></code></p>
             <p class="server-description">版本： 1.21.1</p>
+            <p class="server-notice">需进群加入白名单</p>
         </div>
         <div class="qq-group join-card">
             <div class="qq-group-title">QQ群</div>
-            <p class="qq-group-name">Minecraftの有兽焉</p>
+            <p class="qq-group-name">名称：Minecraftの有兽焉</p>
             <p class="qq-group-id">群号： 971327134</p>
-            <p class="qq-group-link ">加群链接：<a href="https://jq.qq.com/?_wv=1027&k=EcPiJtYh">点此加群</a></p>
+            <a class="route-link auto-link vp-hero-action primary no-external-link-icon"
+                href="https://jq.qq.com/?_wv=1027&k=EcPiJtYh">点此加群</a>
         </div>
     </div>
 </template>
@@ -21,34 +30,29 @@
     gap: 16px;
     justify-content: space-between;
 }
-.join-card{
+
+.join-card {
     display: flex;
     flex-direction: column;
     align-items: flex-start;
+    justify-content: space-between;
     border: 1px solid #ddd;
-    border-radius: 8px;
-    padding: 24px;
 
+    padding: 1.5em;
+    flex: 1;
     box-sizing: border-box;
+
+    margin-block: .75rem;
+    border-radius: .5em;
 }
-.join-card p{
+
+.join-card p {
     line-height: 1.2;
 }
-.server-name, .qq-group-title{
+
+.server-name,
+.qq-group-title {
     font-weight: bold;
-    font-size: 1.2rem;
-}
-
-
-@media (max-width: 768px) {
-    .join-card {
-        flex: 1 1 calc(50% - 16px);
-    }
-}
-
-@media (max-width: 480px) {
-    .join-card {
-        flex: 1 1 100%;
-    }
+    font-size: 1.5rem;
 }
 </style>
